3d756291237b41bcd69bbd89f35d3a7c606cf2ae
ZERONE <zerone012@gmail.com>
2023-08-25 09:39:55 +0900

    UI 공부 중. 상태관리 - GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:study_flutter_13_totally03_lec_getx/countController.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,27 +18,41 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Flutter Demo Home Page'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GetBuilder<CountController>(
+                init: CountController(),
+                builder: (_) => Text(
+                    'Current Count: ${Get.find<CountController>().counter}'),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => Get.find<CountController>().increase(),
+                    child: const Text('증가'),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () => Get.find<CountController>().decrease(),
+                    child: const Text('감소'),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
-      body: const Center(),
     );
   }
 }
